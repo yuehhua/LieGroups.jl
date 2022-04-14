30330e284ee7258fdc56e13e6bbe399d199d18ad
Documenter.jl <documenter@juliadocs.github.io>
2022-04-14 05:34:53 +0000

    build based on 037146f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LieGroup","category":"page"},{"location":"#LieGroup","page":"Home","title":"LieGroup","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LieGroup.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LieGroup]","category":"page"}]
+}
